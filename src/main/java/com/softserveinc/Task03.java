442eca42f486eaddfc7290a7c6c204284f7c3060
Anastasiia Serbokryl-Ustimova <aserbokryl@gmail.com>
2022-01-12 12:20:22 +0200

    Second task is ready

--- src/main/java/com/softserveinc/Task03.java
+++ src/main/java/com/softserveinc/Task03.java
@@ -6,6 +6,13 @@ public class Task03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // TODO: Write your code here
+        System.out.println("How much water do you need?");
+        int number= Integer.parseInt(scanner.nextLine());
+        if (number > 300) {
+        System.out.println("How much milk do you need?");
+            } else {
+            System.out.println("No coffee for today" );
+        }
 
     }
 }
