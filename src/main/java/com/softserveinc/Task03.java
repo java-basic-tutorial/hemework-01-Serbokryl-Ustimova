a62c0d6f5822652641527ff6706209dd274ad750
Anastasiia Serbokryl-Ustimova <aserbokryl@gmail.com>
2022-01-13 21:48:24 +0200

    Third task is ready

--- src/main/java/com/softserveinc/Task03.java
+++ src/main/java/com/softserveinc/Task03.java
@@ -10,16 +10,16 @@ public class Task03 {
         int water = scanner.nextInt();
         System.out.println("Write how many ml of milk the coffee machine has:");
         int milk = scanner.nextInt();
-        System.out.println("Write how many gr of coffee beans the coffee machine has:");
-        int beans = scanner.nextInt();
+       System.out.println("Write how many gr of coffee beans the coffee machine has:");
+       int beans = scanner.nextInt();
         System.out.println("Write how many cups of coffee do you want:");
         int cups = scanner.nextInt();
-        int count = Math.min(water / 200, milk / 50);
-        count = Math.min(count, beans / 15);
-        if (count >= cups) {
-            System.out.println("Yes, I can make that amount of coffee (and even N more than that)");
+//        int count = Math.min(water / 200, milk / 50);
+//        count = Math.min(count, beans / 15);
+        if (water >= 200 && milk >= 50 && beans >= 15) {
+            System.out.println("Yes, I can make " + cups + " of coffee");
         } else {
-            System.out.println("No, I can make only N cup(s) of coffee");
+            System.out.println("No, I can not make " + cups + " of coffee");
         }
     }
     }
